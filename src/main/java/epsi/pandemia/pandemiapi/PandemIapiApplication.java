5dc48cfd164f443481cba36d269146f2566bb16f
psupiot <psupiot44@gmail.com>
2025-03-20 11:41:44 +0100

    initial commit

---
+++ src/main/java/epsi/pandemia/pandemiapi/PandemIapiApplication.java
@@ -0,0 +1,13 @@
+package epsi.pandemia.pandemiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemIapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandemIapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/epsi/pandemia/pandemiapi/PandemIapiApplicationTests.java
@@ -0,0 +1,13 @@
+package epsi.pandemia.pandemiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemIapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
